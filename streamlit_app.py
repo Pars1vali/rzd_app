38b9b5aca987d3de6f98c3548ed241db89219bf4
Chumak Vladimir <parsival427@gmail.com>
2024-05-18 08:38:32 +0300

    Создание проекта

---
+++ streamlit_app.py
@@ -0,0 +1,27 @@
+import io
+import json
+import streamlit as st
+import requests
+
+url="https://pars1vali-rzd-9288.twc1.net/audio"
+
+
+def send_audio(uploaded_file):
+    files = {"file": (uploaded_file.name, uploaded_file.getvalue())}
+    response = requests.post(url, files=files)
+
+    if response.status_code == 200:
+        response_json = json.loads(response.text)
+        st.write(response_json)
+        st.success(f"File '{uploaded_file.name}' uploaded successfully!")
+    else:
+        st.error(f"Failed to upload file. Status code: {response.status_code}")
+
+
+st.title("РЖД для служебных переговоров")
+uploaded_file = st.file_uploader("Загрузите аудио", type=['wav','mp3'] )
+
+if uploaded_file is not None:
+    with st.spinner("Обработка"):
+        send_audio(uploaded_file)
+
