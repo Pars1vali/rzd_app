a8108f4f6fb0d5bee6a22b704308551ab18144e2
Andrey Khachatryan <andrey@omg.lol>
2024-05-18 23:08:12 +0300

    chore: UI update add pretty user-friendly response

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
-import io
 import json
-import streamlit as st
-import requests
 
-url="https://pars1vali-rzd-c40d.twc1.net/audio"
+import requests
+import streamlit as st
+
+url = "https://pars1vali-rzd-c40d.twc1.net/audio"
+
 
 def send_audio(uploaded_file):
     files = {"file": (uploaded_file.name, uploaded_file.getvalue())}
@@ -11,16 +12,34 @@ def send_audio(uploaded_file):
 
     if response.status_code == 200:
         response_json = json.loads(response.text)
-        st.write(response_json)
-        st.success("Файл обработан!")
-    else:
-        st.error(f"Failed to upload file. Status code: {response.status_code}")
+        container = st.container(border=True)
+
+        if response_json["speech_valid"] is True:
+            container.image("assets/happy.png", width=135)
+            container.json(response_json["text"], expanded=False)
+            container.write("Переговоры соответствуют регламенту")
+        else:
+            problems = None
+            for problem in response_json["type_problem"]:
+                if problem == "special_words":
+                    problems = "Использование запрещенных слов"
+                elif problem == "template_error":
+                    problems = "Нарушение шаблона"
+                else:
+                    problems = "Неизвестная причина"
+
+            container.image("assets/sad.png", width=135)
+            container.json(response_json["text"], expanded=False)
+            container.write(
+                f"Переговоры не соответствуют регламенту. Причина: {problems}"
+            )
+
+            # st.json(response_json)
 
 
 st.title("РЖД для служебных переговоров")
-uploaded_file = st.file_uploader("Загрузите аудио", type=['wav','mp3'] )
+uploaded_file = st.file_uploader("Загрузите аудио", type=["wav", "mp3"])
 
 if uploaded_file is not None:
     with st.spinner("Обработка"):
         send_audio(uploaded_file)
-
